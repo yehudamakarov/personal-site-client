b6b82aaa4bf0edcac8a302a6595fc8979d230927
Yehuda Makarov <yehudamakarov@gmail.com>
2019-07-22 15:17:47 -0400

    added code icon

--- src/Root/TopBar/TopBar.tsx
+++ src/Root/TopBar/TopBar.tsx
@@ -1,16 +1,33 @@
 import React from 'react'
-import { TextField, Container, Drawer, Button, AppBar, Toolbar, IconButton, Typography } from "@material-ui/core"
+import { TextField, Container, Drawer, Button, AppBar, Toolbar, IconButton, Typography, makeStyles, createStyles, Theme } from "@material-ui/core"
 import MenuIcon from "@material-ui/icons/Menu";
+import CodeIcon from "@material-ui/icons/Code"
+
+const useStyles = makeStyles((theme: Theme) => 
+     createStyles(
+        {
+            iconPush: {
+                flexGrow: 1
+            }
+        }
+    )
+)
 
 export default function TopBar() {
+    const classes = useStyles();
     return (
         <AppBar color="primary">
-            <Toolbar>
-                <IconButton edge="start" color="inherit" >
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6">YM</Typography>
-            </Toolbar>
+            <Container>
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.iconPush}>YM</Typography>
+                    <IconButton color="inherit">
+                        <CodeIcon />
+                    </IconButton>
+                </Toolbar>
+            </Container>
         </AppBar>
     )
 }
