72bd7bdd394cd5f3fd51de0292fc1b2d565cf71a
Yehuda Makarov <yehudamakarov@gmail.com>
2019-09-10 22:55:55 -0400

    isLoading and isError on projects for homescreen

--- src/root/pages/home/components/cards/projects/HomeCardTextProjects.tsx
+++ src/root/pages/home/components/cards/projects/HomeCardTextProjects.tsx
@@ -1,4 +1,10 @@
-import { Card, Grid, Typography } from "@material-ui/core";
+import {
+    Card,
+    Fade,
+    Grid,
+    LinearProgress,
+    Typography,
+} from "@material-ui/core";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProjectsLoadingAction } from "../../../../../../store/actions/projects/projectsActions";
@@ -13,7 +19,7 @@ const HomepageCardTextProjects = () => {
 
     useEffect(() => {
         dispatch(getProjectsLoadingAction());
-    }, []);
+    }, [dispatch]);
 
     return (
         <Grid container spacing={3}>
@@ -22,11 +28,22 @@ const HomepageCardTextProjects = () => {
             </Grid>
             <Grid item xs={12}>
                 <Grid container spacing={2} direction="column">
-                    {projects.map((project) => (
-                        <Grid item key={project.name}>
-                            <HomeProjectCard project={project} />
+                    {isLoading && <LinearProgress variant="query" />}
+                    {isError && (
+                        <Grid item xs={12} sm={8} md={6}>
+                            <Typography variant="subtitle2" color="error">
+                                There was a problem fetching the information
+                                that is supposed to be displayed here.
+                            </Typography>
                         </Grid>
-                    ))}
+                    )}
+                    {!isLoading &&
+                        !isError &&
+                        projects.map((project) => (
+                            <Grid item key={project.name}>
+                                <HomeProjectCard project={project} />
+                            </Grid>
+                        ))}
                 </Grid>
             </Grid>
         </Grid>
