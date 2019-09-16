7683b8e61bff21b50d8788a04c340fa9511007b2
Yehuda Makarov <yehudamakarov@gmail.com>
2019-09-16 15:25:52 -0400

    .

--- src/root/pages/projects/components/blogPostsByProjectList.tsx
+++ src/root/pages/projects/components/blogPostsByProjectList.tsx
@@ -6,10 +6,10 @@ import {
     makeStyles,
     Theme,
     Typography,
+    useTheme,
 } from "@material-ui/core";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { SpringGrid } from "react-stonecutter";
 
 import { getBlogPostsByProjectIdLoadingAction } from "../../../../store/blogPost/actions/getBlogPostsByProjectId";
 import { IProject } from "../../../../store/projects/types";
@@ -30,9 +30,10 @@ const useStyles = makeStyles((theme: Theme) =>
     })
 );
 
-const Grid = SpringGrid;
 
 export const BlogPostsByProjectList = (props: IOwnProps) => {
+    const theme = useTheme();
+
     const classes = useStyles();
     const dispatch = useDispatch();
     const { project } = props;
@@ -48,34 +49,26 @@ export const BlogPostsByProjectList = (props: IOwnProps) => {
     const blogPostsAreLoading = useSelector((state: IApplicationState) => {
         return state.blogPosts.blogPostUi.allIsLoading;
     });
-    // const blogPostsForProject = useSelector((state: IApplicationState) => {
-    //     if (project) {
-    //         return state.blogPosts.blogPostData.filter((blogPost) => {
-    //             return blogPost.projectId === project.githubRepoDatabaseId;
-    //         });
-    //     } else {
-    //         return [];
-    //     }
-    // });
+    const blogPostsForProject = useSelector((state: IApplicationState) => {
+        if (project) {
+            return state.blogPosts.blogPostData.filter((blogPost) => {
+                return blogPost.projectId === project.githubRepoDatabaseId;
+            });
+        } else {
+            return [];
+        }
+    });
 
-    // const gridItems = blogPostsForProject.map((blogPost) => {
-    //     return <div />;
-    // });
+    const gridItems = blogPostsForProject.map((blogPost, i) => {
+        return <div key={i}><Card><Typography variant="h6">{blogPost.title}</Typography></Card></div>;
+    });
 
     return (
-        <div className={classes.listDiv}>
-            <Typography className={classes.listTitle} variant="h5">
+        <div >
+            <Typography variant="h5">
                 Posts for this project:
             </Typography>
-            {blogPostsAreLoading ? (
-                <LinearProgress variant="query" />
-            ) : (
-                <SpringGrid>
-                    {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((el, i) => {
-                        return <div>{el}</div>;
-                    })}
-                </SpringGrid>
-            )}
+            {gridItems}
         </div>
     );
 };
