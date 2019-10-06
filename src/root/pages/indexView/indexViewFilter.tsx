7bfbb874934de108e3d2ca1cf7425810e250e0e0
Yehuda Makarov <yehudamakarov@gmail.com>
2019-10-06 15:37:32 -0400

    switched to multi select, need styling

--- src/root/pages/indexView/indexViewFilter.tsx
+++ src/root/pages/indexView/indexViewFilter.tsx
@@ -1,12 +1,18 @@
 import {
     Checkbox,
+    FormControl,
     FormControlLabel,
     FormGroup,
+    Input,
+    InputLabel,
+    ListItemText,
+    MenuItem,
+    Select,
     Switch,
 } from "@material-ui/core";
 import React from "react";
 import { useDispatch } from "react-redux";
-import { IFilter } from "../../../store/ui/IUiState";
+import { IFilter, IFilterListingTypes } from "../../../store/ui/IUiState";
 import { setFilterAction } from "../../../store/ui/uiActions";
 
 const IndexViewFilter = React.memo((props: { filter: IFilter }) => {
@@ -16,12 +22,20 @@ const IndexViewFilter = React.memo((props: { filter: IFilter }) => {
     } = props;
     const dispatch = useDispatch();
 
-    const handleListingTypeChange = (
-        listingTypeSwitched: "projects" | "blogPosts" | "tags"
-    ) => (event: React.ChangeEvent<HTMLInputElement>) => {
-        const checked = event.target.checked;
+    const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+        const selectedValues = event.target.value as string[];
+        const newListingTypes: IFilterListingTypes = { ...listingTypes };
+        for (const listingType in listingTypes) {
+            if (listingTypes.hasOwnProperty(listingType)) {
+                if (selectedValues.includes(listingType)) {
+                    newListingTypes[listingType] = true;
+                } else {
+                    newListingTypes[listingType] = false;
+                }
+            }
+        }
         const newFilter: IFilter = {
-            listingTypes: { ...listingTypes, [listingTypeSwitched]: checked },
+            listingTypes: newListingTypes,
             searchText: filter.searchText,
             tagIds: filter.tagIds,
         };
@@ -29,37 +43,63 @@ const IndexViewFilter = React.memo((props: { filter: IFilter }) => {
         dispatch(setFilterAction(newFilter));
     };
 
+    const getArrayOfListingTypesSelected = () => {
+        const keys: string[] = Object.keys(listingTypes);
+        return keys.reduce(
+            (agg, el) => {
+                if (listingTypes[el]) {
+                    agg.push(el);
+                }
+                return agg;
+            },
+            [] as string[]
+        );
+    };
+
+    const listingTypeValues = getArrayOfListingTypesSelected();
+
+    const getSelectedAsDisplayString = (selected: any) => {
+        return (selected as ["projects", "blogPosts", "tags"])
+            .map((listingType) => {
+                switch (listingType) {
+                    case "projects":
+                        return "Projects";
+                    case "blogPosts":
+                        return "Blog Posts";
+                    case "tags":
+                        return "Tags";
+                    default:
+                        break;
+                }
+            })
+            .join(", ");
+    };
+
     return (
         <div>
-            <FormGroup row>
-                <FormControlLabel
-                    control={
-                        <Checkbox
-                            checked={listingTypes.projects}
-                            onChange={handleListingTypeChange("projects")}
-                        />
-                    }
-                    label="Projects"
-                />
-                <FormControlLabel
-                    control={
-                        <Checkbox
-                            checked={listingTypes.blogPosts}
-                            onChange={handleListingTypeChange("blogPosts")}
-                        />
-                    }
-                    label="Blog Posts"
-                />
-                <FormControlLabel
-                    control={
-                        <Checkbox
-                            checked={listingTypes.tags}
-                            onChange={handleListingTypeChange("tags")}
-                        />
-                    }
-                    label="Tags"
-                />
-            </FormGroup>
+            <FormControl>
+                <InputLabel htmlFor="show-multiple-select">Showing:</InputLabel>
+                <Select
+                    multiple
+                    value={listingTypeValues}
+                    onChange={handleChange}
+                    renderValue={getSelectedAsDisplayString}
+                    input={<Input id="show-multiple-select" />}
+                >
+                    <MenuItem value="projects">
+                        <Checkbox checked={listingTypes.projects} />
+                        <ListItemText primary="Projects" />
+                    </MenuItem>
+                    <MenuItem value="blogPosts">
+                        <Checkbox checked={listingTypes.blogPosts} />
+                        <ListItemText primary="Blog Posts" />
+                    </MenuItem>
+                    <MenuItem value="tags">
+                        <Checkbox checked={listingTypes.tags} />
+                        <ListItemText primary="Tags" />
+                    </MenuItem>
+                </Select>
+            </FormControl>
         </div>
     );
 });
