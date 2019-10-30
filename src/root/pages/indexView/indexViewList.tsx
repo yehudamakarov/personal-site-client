783aeebe5593c773a2f85e7ecd33057de6ab2566
Yehuda Makarov <yehudamakarov@gmail.com>
2019-10-30 09:29:35 -0400

    refactoring

--- src/root/pages/indexView/indexViewList.tsx
+++ src/root/pages/indexView/indexViewList.tsx
@@ -2,11 +2,12 @@ import { Grid } from "@material-ui/core";
 import _ from "lodash";
 import React, { useEffect } from "react";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
+import { FilterHelpers } from "../../../helpers/filterHelpers";
 import { getBlogPostsLoadingAction } from "../../../store/actions/blogPost/getBlogPosts/actions";
 import { getProjectsLoadingAction } from "../../../store/actions/projects/getProjects/actions";
 import { IApplicationState } from "../../../store/rootReducer";
+import { IFilterListingTypes } from "../../../store/ui/IUiState";
 import IndexViewCard from "./indexViewCard";
-import { filterBlogPosts, filterProjects } from "./indexViewListHelpers";
 
 const IndexViewList = () => {
     const dispatch = useDispatch();
@@ -31,50 +32,60 @@ const IndexViewList = () => {
     const currentFilter = { listingTypes, searchText, tagIds };
 
     const filteredBlogPosts = useSelector(
-        filterBlogPosts(currentFilter),
-        _.isEqual,
+        FilterHelpers.filterIndexEntities(
+            currentFilter,
+            (filterListingTypes) => filterListingTypes.blogPosts,
+            (state) => state.blogPosts.blogPostData,
+            (blogPost) => blogPost.title
+        ),
+        _.isEqual
     );
 
     const filteredProjects = useSelector(
-        filterProjects(currentFilter),
-        _.isEqual,
+        FilterHelpers.filterIndexEntities(
+            currentFilter,
+            (filterListingTypes: IFilterListingTypes) =>
+                filterListingTypes.projects,
+            (state: IApplicationState) => state.projects.projectsData,
+            (project) => project.projectTitle
+        ),
+        _.isEqual
     );
 
     const getCards = () => {
         const blogPostCards = filteredBlogPosts
             ? filteredBlogPosts.map((blogPost) => {
-                return (
-                    <IndexViewCard
-                        type="blogPost"
-                        githubLink={null}
-                        regularLink={`/blogPosts/${blogPost.slug}`}
-                        key={blogPost.id}
-                        title={blogPost.title}
-                        subTitle={blogPost.description}
-                        tagIds={blogPost.tagIds}
-                    />
-                );
-            })
+                  return (
+                      <IndexViewCard
+                          type="blogPost"
+                          githubLink={null}
+                          regularLink={`/blogPosts/${blogPost.slug}`}
+                          key={blogPost.id}
+                          title={blogPost.title}
+                          subTitle={blogPost.description}
+                          tagIds={blogPost.tagIds}
+                      />
+                  );
+              })
             : [];
 
         const projectCards = filteredProjects
             ? filteredProjects.map((project) => {
-                return (
-                    <IndexViewCard
-                        type="project"
-                        githubLink={project.githubUrl}
-                        regularLink={`/projects/${project.slug}`}
-                        key={project.githubRepoDatabaseId}
-                        title={project.projectTitle}
-                        subTitle={project.projectDescription}
-                        tagIds={project.tagIds}
-                    />
-                );
-            })
+                  return (
+                      <IndexViewCard
+                          type="project"
+                          githubLink={project.githubUrl}
+                          regularLink={`/projects/${project.slug}`}
+                          key={project.githubRepoDatabaseId}
+                          title={project.projectTitle}
+                          subTitle={project.projectDescription}
+                          tagIds={project.tagIds}
+                      />
+                  );
+              })
             : [];
 
-        const allCards = blogPostCards.concat(projectCards);
-        return allCards;
+        return blogPostCards.concat(projectCards);
     };
 
     return (
